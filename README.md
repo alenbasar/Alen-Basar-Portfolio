c677ac62eaa84f93a9fa77062b6277710eb004d6
Alen <alenbasar@outlook.com>
2021-09-11 22:46:01 +1000

    Fixed sizing issues.

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# Alen Basar Portfolio
-## Description
-This is my portfolio website that I made using TypeScript, ReactJS and SCSS, feel free to check it out :) https://alenbasar.netlify.app/
-![Home](https://user-images.githubusercontent.com/63575553/131985705-1fec444a-2959-482a-a3a3-11b048c4a9d5.jpg)
-![About](https://user-images.githubusercontent.com/63575553/131985646-13e4bf03-8835-47bd-9290-e2cbf08c6727.jpg)
-![Code](https://user-images.githubusercontent.com/63575553/131985648-924e610d-d7b7-414d-bb2a-d5c01454cc2d.JPG)
+
--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -23,28 +23,28 @@ export type MenuItem = {
 
 export const hamburgerIcon: MenuItem[] = [];
 export const mobileMenu: MenuItem[] = [
-  { key: 'm-home', to: 'home', label: 'HOME' },
-  { key: 'm-about', to: 'about', label: 'ABOUT ME' },
-  { key: 'm-projects', to: 'showcase', label: 'MY_PROJECTS' },
-  { key: 'm-contact', to: 'contact', label: 'CONTACT' },
-  { key: 'm-resume', link: '', label: 'RESUME' },
+  { key: "m-home", to: "home", label: "HOME" },
+  { key: "m-about", to: "about", label: "ABOUT ME" },
+  { key: "m-projects", to: "showcase", label: "MY_PROJECTS" },
+  { key: "m-contact", to: "contact", label: "CONTACT" },
+  { key: "m-resume", link: "", label: "RESUME" },
 ];
 
 export const navLogo: MenuItem[] = [];
 export const desktopMenu: MenuItem[] = [
-  { key: 'home', to: 'home', label: 'Home' },
-  { key: 'about', to: 'about', label: 'About' },
-  { key: 'projects', to: 'showcase', label: 'Projects' },
-  { key: 'contact', to: 'contact', label: 'Contact' },
+  { key: "home", to: "home", label: "Home" },
+  { key: "about", to: "about", label: "About" },
+  { key: "projects", to: "showcase", label: "Projects" },
+  { key: "contact", to: "contact", label: "Contact" },
   {
-    key: 'resume',
-    link: 'https://storage.googleapis.com/portfolio_take1/Alen_Basar-Resume.pdf',
-    label: 'Resume',
+    key: "resume",
+    link: "https://alenbasar.s3.ap-southeast-2.amazonaws.com/Alen_Basar-Resume.pdf",
+    label: "Resume",
   },
 ];
 
 export const footerMenu: MenuItem[] = [
-  { key: 'f-github', link: '', label: 'GitHub' },
-  { key: 'f-linkedin', link: '', label: 'LinkedIn' },
-  { key: 'f-resume', link: '', label: 'My Resume' },
+  { key: "f-github", link: "", label: "GitHub" },
+  { key: "f-linkedin", link: "", label: "LinkedIn" },
+  { key: "f-resume", link: "", label: "My Resume" },
 ];
