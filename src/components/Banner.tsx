0bcd38101c438b04858b9783715f327087ded9a7
Alen Basar <alenbasar@outlook.com>
2022-01-13 22:47:12 +1100

    fixed padding

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -39,67 +39,6 @@ const Banner = (props: Props) => {
           {cta}
         </Link>
       </div>
-      {/* <Particles
-        className="c-banner__particles"
-        params={{
-          particles: {
-            number: {
-              value: 80,
-              density: {
-                enable: true,
-                value_area: 900,
-              },
-            },
-            size: {
-              value: 2,
-            },
-            color: {
-              value: "#b5179e",
-            },
-            lineLinked: {
-              enable: true,
-              distance: 210,
-              color: "#b5179e",
-              opacity: 1,
-              width: 1.3,
-            },
-            shape: {
-              polygon: {
-                nb_sides: 4,
-              },
-            },
-            opacity: {
-              value: 1,
-              random: true,
-              anim: {
-                enable: false,
-                speed: 1,
-                opacity_min: 0.1,
-                sync: false,
-              },
-            },
-            move: {
-              enable: true,
-              speed: 1,
-              bounce: true,
-              outMode: "bounce",
-            },
-          },
-          interactivity: {
-            events: {
-              onhover: {
-                enable: false,
-                mode: "repulse",
-              },
-            },
-          },
-          polygon: {
-            move: {
-              radius: 10,
-            },
-          },
-        }}
-      /> */}
     </section>
   );
 };
